3fbbf61d8a20d99e8a0eb4d0d7832fbe40e94a39
supahxyt <josuefirelol@gmail.com>
2021-04-25 03:02:43 -0700

    No resize hint and statusbar finalized
    dmenu font specified aswell

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Jet Brains Mono:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Jet Brains Mono:pixelsize=13:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Jet Brains Mono:pixelsize=13:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#2b303b";
 static const char col_gray3[]       = "#bbbbbb";
@@ -33,10 +33,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-// Originially 0.55
-static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -46,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
--- status.sh
+++ status.sh
@@ -5,7 +5,8 @@ do
 	sig=$(awk 'END{print$4}' /proc/net/wireless | sed 's/^/ /;s/\./ dBm/')
 	mem=$(free -t | grep "Mem" | awk '{ printf("%.2f%\n"), $3/$2 * 100}')
 	cpu=$($HOME/git/dots/dwm/dwmstatus/cpu.o)
-	bat=$($HOME/test.o)
-	dwm -s "$(dwmstatus | sed "s/|/\[${sig}\] \[ ${cpu}\] \[ ${mem}\] \[${bat}\]/")"
+#	bat=$($HOME/test.o)
+#	dwm -s "$(dwmstatus | sed "s/|/\[${sig}\] \[ ${cpu}\] \[ ${mem}\] \[${bat}\]/")"
+	dwm -s "$(dwmstatus | sed "s/|/\[${sig}\]\[ ${cpu}\]\[ ${mem}\]/")"
 done
 
